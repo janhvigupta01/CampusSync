700905e42f550ed9a105e7fb9c861141836d82a5
janhvigupta01 <janhvigupta5555@gmail.com>
2026-07-20 14:16:07 +0530

    Initial commit

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,20 +1,18 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: 'crop-38fc3.firebaseapp.com',
-  projectId: 'crop-38fc3',
-  storageBucket: 'crop-38fc3.firebasestorage.app',
-  messagingSenderId: '416242623276',
-  appId: '1:416242623276:web:fa8f397e807e7d7c798289',
-}
+  authDomain: "campussync-d9d41.firebaseapp.com",
+  projectId: "campussync-d9d41",
+  storageBucket: "campussync-d9d41.firebasestorage.app",
+  messagingSenderId: "1001183109674",
+  appId: "1:1001183109674:web:2436ebe7541e1df1af9953",
+  measurementId: "G-9212WPM26N",
+};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
-
-export { app, auth, provider }
+export { app, auth, provider };
